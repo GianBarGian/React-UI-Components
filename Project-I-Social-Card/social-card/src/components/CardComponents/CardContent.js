69852d6653a313f3d9d3a951e7f668ffd899bda4
Giacomo Benati <giacomobenati@mailbox.org>
2019-02-11 15:25:53 +0100

    (MVP) added react componensts for the first project

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Card.css';
+
+
+export default function CardContent() {
+    return [
+        <h2>Get started with React</h2>,
+        <p> React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>,
+        <p><span>reactjs.org</span></p>,
+    ]
+}
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+export default function HeaderTitle() {
+    const newDate = new Date();
+    return (
+        [
+            <h1>Lambda School</h1>,
+            <p>@LambdaSchool</p>,
+            <p>{newDate.toString()}</p>,
+        ]
+    )
+}
+
